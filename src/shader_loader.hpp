f4e7e787b345d6354fd7d7adade1f136ac5783c0
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2014-06-02 17:43:02 +0100

    - Modified license advice so now the project is under the GPL v3 license, without the "any later version" clause.

--- src/shader_loader.hpp
+++ src/shader_loader.hpp
@@ -4,9 +4,8 @@
  * This file is part of MSL.
  *
  * MSL is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
+ * it under the terms of the GNU General Public License v3 as published by
+ * the Free Software Foundation.
  *
  * MSL is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
